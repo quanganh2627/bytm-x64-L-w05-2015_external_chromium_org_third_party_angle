baabec0f392e4927dd68fb75154223abd4e1ffe6
Android Chromium Automerger <chromium-automerger@android>
2014-03-01 04:08:41 +0000

    Update makefiles after merge of Chromium at 254343
    This commit was generated by merge_from_chromium.py.
    
    Change-Id: I0dbfc466bdfabb24bae590b35240434b5997bf42

--- src/translator.target.linux-arm.mk
+++ src/translator.target.linux-arm.mk
@@ -40,6 +40,7 @@ LOCAL_SRC_FILES := \
 	third_party/angle/src/compiler/translator/InitializeVariables.cpp \
 	third_party/angle/src/compiler/translator/IntermTraverse.cpp \
 	third_party/angle/src/compiler/translator/Intermediate.cpp \
+	third_party/angle/src/compiler/translator/LoopInfo.cpp \
 	third_party/angle/src/compiler/translator/MapLongVariableNames.cpp \
 	third_party/angle/src/compiler/translator/OutputESSL.cpp \
 	third_party/angle/src/compiler/translator/OutputGLSL.cpp \
